b98323255d767663327eed5908d5d9a8391a1a15
Jasmine Tan <jasmine.tan6032@gmail.com>
2020-11-12 13:11:29 +0000

    config file adapted

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/jasminetan6032/artemis_checklist/master/protocols/artemis_schema.jsonld',
+  githubSrc: 'https://raw.githubusercontent.com/jasminetan6032/artemis_checklist/master/protocols/artemis_schema',
   banner: 'This protocol is under construction.',
   startButton: 'Start',
   assetsPublicPath: '/artemis_checklist/',
